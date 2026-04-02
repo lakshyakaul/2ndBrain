8dc5be1fd12798c91a95d2505cfd911317f68211
Lakshya Kaul <lakshyakaul05@gmail.com>
2026-04-02 18:38:53 +0530

    nextjs, supabase, drizzle setup, schema creations and migrations, start of ui setup

---
+++ project/drizzle.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not defined");
+}
+
+export default defineConfig({
+    schema: "./src/lib/supabase/schema.ts",
+    out: "./migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL || '',
+    },
+});
